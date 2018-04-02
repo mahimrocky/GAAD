4f9274b079f61e1f74442cbd905ccf05c065f19e
tariqul <tariqul@w3engineers.com>
2018-04-02 19:21:58 +0600

    constrain layout adding. It will be continued

---
+++ app/src/main/java/com/aad/core/gaad/ui_and_ux/constrain_layout/ConstrainLayoutTestActivity.java
@@ -0,0 +1,15 @@
+package com.aad.core.gaad.ui_and_ux.constrain_layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aad.core.gaad.R;
+
+public class ConstrainLayoutTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constrain_layout_test);
+    }
+}
